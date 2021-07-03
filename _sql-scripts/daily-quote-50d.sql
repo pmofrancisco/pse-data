cc1859a1c98a47e7a2adf2d5195d2ad94c66b3fd
Paul Michael Francisco <pmofrancisco@yahoo.com>
2021-07-03 16:39:19 +0800

    Trend following scripts

--- _sql-scripts/daily-quote-50d.sql
+++
@@ -1,8 +0,0 @@
-select
-	StockCode,
-	avg(ClosePrice) as SMA50, max(ClosePrice) as ClosePrice50D
-from
-	(select *, ROW_NUMBER() over(partition by StockCode order by QuoteDate desc) as RowNumber
-	from DailyQuote as DailyQuoteWithRowNumber) as DailyQuoteWithRowNumber
-where RowNumber <= 50
-group by StockCode
